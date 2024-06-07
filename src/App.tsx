0cbb43eb928e51020829068ff721f8927ac2fafa
Anastasiia Manchenko <45871835+kukutish@users.noreply.github.com>
2024-06-07 17:36:14 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -51,36 +51,23 @@ const App: React.FC = () => (
     <IonApp>
       <IonReactRouter>
         <IonTabs>
-          <HashRouter>
-            <IonRouterOutlet>
-              <Route path="/tab1">
-                <Tab1 />
-              </Route>
-              <Route path="/tab2">
-                <Tab2 />
-              </Route>
-              <Route path="/tab3">
-                <Tab3 />
-              </Route>
-              <Route exact path="/">
-                <Redirect to="/tab1" />
-              </Route>
-            </IonRouterOutlet>
-            <IonTabBar slot="bottom">
-              <IonTabButton tab="tab1" href="/tab1">
-                <IonIcon aria-hidden="true" icon={triangle} />
-                <IonLabel>Tab 1</IonLabel>
-              </IonTabButton>
-              <IonTabButton tab="tab2" href="/tab2">
-                <IonIcon aria-hidden="true" icon={ellipse} />
-                <IonLabel>Tab 2</IonLabel>
-              </IonTabButton>
-              <IonTabButton tab="tab3" href="/tab3">
-                <IonIcon aria-hidden="true" icon={square} />
-                <IonLabel>Tab 3</IonLabel>
-              </IonTabButton>
-            </IonTabBar>
-          </HashRouter>
+          <IonRouterOutlet>
+            <Route path="/tab1" component={Tab1} />
+            <Route path="/tab2" component={Tab2} />
+            <Route path="/tab3" component={Tab3} />
+            <Redirect exact path="/" to="/tab1" />
+          </IonRouterOutlet>
+          <IonTabBar slot="bottom">
+            <IonTabButton tab="tab1" href="/tab1">
+              <IonLabel>Tab 1</IonLabel>
+            </IonTabButton>
+            <IonTabButton tab="tab2" href="/tab2">
+              <IonLabel>Tab 2</IonLabel>
+            </IonTabButton>
+            <IonTabButton tab="tab3" href="/tab3">
+              <IonLabel>Tab 3</IonLabel>
+            </IonTabButton>
+          </IonTabBar>
         </IonTabs>
       </IonReactRouter>
     </IonApp>
